77ef5b77bf6a4ea3a5fa0aae31743742c4e01d9a
krasjbee <zloyjb@gmail.com>
2021-10-22 00:00:29 +0700

    refactor : some cleanup

--- app/src/main/java/com/example/margarettipizza/presentation/menu/PizzaViewHolder.kt
+++ app/src/main/java/com/example/margarettipizza/presentation/menu/PizzaViewHolder.kt
@@ -15,21 +15,14 @@ class PizzaViewHolder(private val binding: PizzaListCardBinding) :
         with(binding) {
             tvPizzaName.text = pizza.name
             tvPizzaDescription.text = pizza.description
-            //TODO format string
             tvPizzaPrice.text = String.format(
                 itemView.context.getString(R.string.ruble_symbol),
                 pizza.price.toInt()
             )
-            //TODO add picture load
-            //sivPizzaPic.setImageDrawable()
             //TODO move picture download to viewmodel
             Glide.with(this.root)
                 .load(pizza.imageUrl).into(sivPizzaPic)
             sivPizzaPic.setOnClickListener {
-                onClick(pizza)
-            }
-            // FIXME: 19.10.2021 DELETE
-            root.setOnClickListener {
                 onClick(pizza)
             }
         }
