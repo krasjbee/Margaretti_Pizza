95fbbb4723ace2f2ac1c65ef9a4e04d3e3a18366
krasjbee <zloyjb@gmail.com>
2021-11-07 00:43:05 +0700

    fix : removed unused usecases

--- app/src/main/java/com/example/margarettipizza/domain/usecase/PizzaDetailsUsecase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.margarettipizza.domain.usecase
-
-//import com.example.margarettipizza.data.repository.PizzaRepositoryImpl
-import com.example.margarettipizza.data.repository.PizzaRepository
-import javax.inject.Inject
-
-class PizzaDetailsUsecase @Inject constructor(private val repository: PizzaRepository) {
-    operator fun invoke(id: Int) = repository.getPizzaById(id)
-}
--- app/src/main/java/com/example/margarettipizza/presentation/preview/PreviewViewModel.kt
+++ app/src/main/java/com/example/margarettipizza/presentation/preview/PreviewViewModel.kt
@@ -1,15 +1,15 @@
 package com.example.margarettipizza.presentation.preview
 
 import androidx.lifecycle.ViewModel
-import com.example.margarettipizza.domain.usecase.PizzaDetailsUsecase
+import com.example.margarettipizza.domain.usecase.PizzaUsecase
 import javax.inject.Inject
 
-class PreviewViewModel @Inject constructor(private val usecase: PizzaDetailsUsecase) : ViewModel() {
+class PreviewViewModel @Inject constructor(private val usecase: PizzaUsecase) : ViewModel() {
 
     fun addToCard(id: Int) {
 
     }
 
 
-    fun getPizzaById(id: Int) = usecase(id)
+    fun getPizzaById(id: Int) = usecase.getPizzaById(id)
 }
