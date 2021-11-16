b24825279cf729d5554ceddcbbe9e983ee4aa3d1
krasjbee <zloyjb@gmail.com>
2021-11-16 15:18:03 +0700

    fix : fixed bug when nothing is shown after fresh install

--- app/src/main/java/com/example/margarettipizza/domain/usecase/PizzaUsecase.kt
+++ app/src/main/java/com/example/margarettipizza/domain/usecase/PizzaUsecase.kt
@@ -4,6 +4,7 @@ import com.example.margarettipizza.data.remote.dto.PizzaDto
 import com.example.margarettipizza.data.repository.local.pizzaRepository.PizzaLocalRepository
 import com.example.margarettipizza.data.repository.remote.PizzaRepository
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.core.SingleSource
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import javax.inject.Inject
 
@@ -15,12 +16,16 @@ class PizzaUsecase @Inject constructor(
     private val listDisposable = CompositeDisposable()
 
     fun getAllPizza(): Single<List<PizzaDto>> {
-        remoteRepository.getAll()
-            .subscribe({
-                listDisposable.add(localRepository.addToDatabase(it).subscribe())
-            }, {
-            }, listDisposable)
-        return localRepository.getAllPizza()
+//        return remoteRepository.getAll().flatMapCompletable {
+//            localRepository.addToDatabase(it)
+//        }.andThen(SingleSource {
+//            localRepository.getAllPizza()
+//        })
+        return remoteRepository.getAll().flatMapCompletable {
+            localRepository.addToDatabase(it)
+        }.andThen(SingleSource { obs ->
+            localRepository.getAllPizza().subscribe({ obs.onSuccess(it) }, {}, listDisposable)
+        })
     }
 
     fun getPizzaByName(query: String): Single<List<PizzaDto>> {
